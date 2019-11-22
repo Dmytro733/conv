3e3e7b8b48e6fc92cac51f48be548ae023c089f0
Dmitro Daneluk <ddp0469@gmail.com>
2019-11-23 00:20:04 +0200

    link: reletive on absolute

--- lesson2/Untitled.js
+++ lesson2/Untitled.js
@@ -3,13 +3,13 @@ var uan = document.querySelector('#UAN'),
     eur = document.querySelector('#EUR');
 
 
-uan.addEventListener('input',()  =>{
+uan.addEventListener('input',()=>{
     let request = new XMLHttpRequest();
 
-    request.open('GET', 'http://127.0.0.1:5500/conve.json');
+    request.open('GET', 'http://127.0.0.1:5500/conver.json');
     request.send();
 
-    request.addEventListener('readystatechange',function(){
+    request.addEventListener('readystatechange',()=>{
         if(request.readyState == 4 && request.status == 200 ){
             let data = JSON.parse(request.response);
 
@@ -19,18 +19,20 @@ uan.addEventListener('input',()  =>{
             usd.value = 'Щось пішло не так';
             eur.value = 'Щось пішло не так';
         }
+        console.log(request.response);
     });
 });
 
-usd.addEventListener('input',()  =>{
-    let request = new XMLHttpRequest();
 
-    request.open('GET', 'http://127.0.0.1:5500/conve.json');
-    request.send();
+usd.addEventListener('input',()=>{
+    let request1 = new XMLHttpRequest();
 
-    request.addEventListener('readystatechange',function(){
-        if(request.readyState == 4 && request.status == 200 ){
-            let data = JSON.parse(request.response);
+    request1.open('GET', 'http://127.0.0.1:5500/conver.json');
+    request1.send();
+
+    request1.addEventListener('readystatechange',()=>{
+        if(request1.readyState == 4 && request1.status == 200 ){
+            let data = JSON.parse(request1.response);
 
             uan.value = (usd.value * data.uan).toFixed(2);
             eur.value = (usd.value * data.EUR).toFixed(2);
@@ -41,15 +43,15 @@ usd.addEventListener('input',()  =>{
     });
 });
 
-eur.addEventListener('input',()  =>{
-    let request = new XMLHttpRequest();
+eur.addEventListener('input',()=>{
+    let request2 = new XMLHttpRequest();
 
-    request.open('GET', 'http://127.0.0.1:5500/conve.json');
-    request.send();
+    request2.open('GET', 'http://127.0.0.1:5500/conver.json');
+    request2.send();
 
-    request.addEventListener('readystatechange',function(){
-        if(request.readyState == 4 && request.status == 200 ){
-            let data = JSON.parse(request.response);
+    request2.addEventListener('readystatechange',()=>{
+        if(request2.readyState == 4 && request2.status == 200 ){
+            let data = JSON.parse(request2.response);
 
             uan.value = (eur.value * data.UAN).toFixed(2);
             usd.value =(eur.value * data.USD).toFixed(2);
