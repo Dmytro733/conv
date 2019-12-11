4b91ed1a2f1cdee02c1a8c5e18a4ea8f66732529
Dmitro Daneluk <ddp0469@gmail.com>
2019-12-11 12:27:25 +0200

    Changes in code: not worked currency calculation

--- lesson2/Untitled.js
+++ lesson2/Untitled.js
@@ -6,34 +6,35 @@ var uan = document.querySelector('#UAN'),
 uan.addEventListener('input',()=>{
     let request = new XMLHttpRequest();
 
-    request.open('GET', 'http://127.0.0.1:5500/conver.json');
-    request.send();
-
+    request.open('GET', './conver.json');
+    request.send()
+    
     request.addEventListener('readystatechange',()=>{
-        if(request.readyState == 4 && request.status == 200 ){
-            let data = JSON.parse(request.response);
+        if(request.readyState == 4 && request.status == 200){
+            let data = JSON.parse(request.responseText);
 
-            usd.value = (uan.value / data.usd).toFixed(2);
-            eur.value = (uan.value / data.eur).toFixed(2);
-        }else{
-            usd.value = 'Щось пішло не так';
-            eur.value = 'Щось пішло не так';
-        }
-        console.log(request.response);
+                usd.value = (uan.value / data.usd).toFixed(2);
+                eur.value = (uan.value / data.eur).toFixed(2);
+            }else{
+                usd.value = 'Щось пішло не так';
+                eur.value = 'Щось пішло не так';
+        }  
     });
 });
-
+uan.addEventListener('click',()=>{
+    return uan.value = ''
+})
 
 usd.addEventListener('input',()=>{
     let request1 = new XMLHttpRequest();
 
-    request1.open('GET', 'http://127.0.0.1:5500/conver.json');
+    request1.open('GET', './conver.json');
     request1.send();
 
     request1.addEventListener('readystatechange',()=>{
         if(request1.readyState == 4 && request1.status == 200 ){
             let data = JSON.parse(request1.response);
-
+        
             uan.value = (usd.value * data.uan).toFixed(2);
             eur.value = (usd.value * data.EUR).toFixed(2);
         }else{
@@ -42,18 +43,21 @@ usd.addEventListener('input',()=>{
         }
     });
 });
+usd.addEventListener('click',()=>{
+    return usd.value = ''
+})
 
 eur.addEventListener('input',()=>{
     let request2 = new XMLHttpRequest();
 
-    request2.open('GET', 'http://127.0.0.1:5500/conver.json');
+    request2.open('GET', './conver.json');
     request2.send();
 
     request2.addEventListener('readystatechange',()=>{
-        if(request2.readyState == 4 && request2.status == 200 ){
+        if(request2.readyState == 4 && request2.status == 200){
             let data = JSON.parse(request2.response);
-
-            uan.value = (eur.value * data.UAN).toFixed(2);
+        
+            uan.value = (eur.value * data.eur).toFixed(2);
             usd.value =(eur.value * data.USD).toFixed(2);
         }else{
             uan.value = 'Щось пішло не так';
@@ -61,7 +65,9 @@ eur.addEventListener('input',()=>{
         }
     });
 });
-
+eur.addEventListener('click',()=>{
+    eur.value = ''
+})
 
 
 
